d8404f375577684ae5f68f76cdf261f28f589def
DESKTOP-1M8BI2T\USER <farouqokeniyi2004@gmail.com>
2024-06-13 00:19:03 +0100

    array learning

---
+++ arraylearning/js/index.js
@@ -0,0 +1,33 @@
+console.log("hello");
+let names = ["ade", "ola", "david", "seun", "david", ['pew', 'ohhh']];
+let name2 = ["ade1", "ola2", "david3", "seun4", "david5"];
+let nums = [1,2,3,4,5,6,7];
+let multu = nums.map((n)=> n*2)
+// let namescopy = [...names];
+// let stringnames = names.toString();
+// let concatination = names.concat(name2) 
+// let mutate=names;
+// names.splice(1,2, 'rice', 'water');
+
+// for each method, i tried to look for an element and print the value
+
+// name2.forEach((n)=>{
+   
+//     if (n=="david3"){
+//         let nums = name2.indexOf(n);
+//         console.log(n)
+//         console.log(nums)
+//     }
+   
+// })
+let arr = ["hello"];
+let spli = arr.split('');
+let text = '1';
+let numss = Number(text);
+// console.log(numss)
+
+// let joinnames = concatination.join(' and ');
+console.log(spli)
+// console.log(stringnames);
+// console.log(joinnames);
+// console.log(concatination);
